a066133310a75f552f27629f1e496df5486bb3f7
Lester Thomas <lester.thomas@vodafone.com>
2021-03-02 16:23:13 +0000

    Moved PartyRole defn from coreFunction to Security

--- examples/Security-Role/builddockerfile.sh
+++ examples/Security-Role/builddockerfile.sh
@@ -1,5 +1,5 @@
-docker build -t lesterthomas/productcatalogapi:0.1 -t lesterthomas/productcatalogapi:latest -f prodcat-dockerfile . 
+docker build -t lesterthomas/productcatalogapi:0.2 -t lesterthomas/productcatalogapi:latest -f prodcat-dockerfile . 
 docker push lesterthomas/productcatalogapi --all-tags
-docker build -t lesterthomas/partyroleapi:0.1 -t lesterthomas/partyroleapi:latest -f partyrole-dockerfile . 
+docker build -t lesterthomas/partyroleapi:0.2 -t lesterthomas/partyroleapi:latest -f partyrole-dockerfile . 
 docker push lesterthomas/partyroleapi --all-tags
 
